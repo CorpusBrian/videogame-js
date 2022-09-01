42b407cac649f735e2a61ba24dd607f1baad0b86
Brian Corpus <corpusbrian@hotmail.com>
2022-09-01 20:09:33 -0300

    Se crea el html, css y js base del juego

---
+++ game.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+
+
+
+}
---
+++ maps.js
@@ -0,0 +1,61 @@
+const emojis = {
+    "-": " ",
+    O: "🚪",
+    X: "💣",
+    I: "🎁",
+    PLAYER: "💀",
+    BOMB_COLLISION: "🔥",
+    GAME_OVER: "👎",
+    WIN: "🏆",
+  };
+  const maps = [];
+  maps.push(`
+    IXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    OXXXXXXXXX
+  `);
+  maps.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+    `);
+  maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+  `);
+  maps.push(`
+    O--XXXXXXX
+    XX--XXXXXX
+    XXX--XXXXX
+    XXXX--XXXX
+    XXXXX--XXX
+    XXXXX--XXX
+    XXXX--XXXX
+    XXX--XXXXX
+    X--XXXXXXX
+    I-XXXXXXXX
+  `);
+
+  
